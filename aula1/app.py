21c2299d62ae24843f566a224331026b737d8c4b
Pedro Xavier <105009132+OxeMonoYuumi@users.noreply.github.com>
2025-02-17 22:37:51 -0300

    Add files via upload

--- aula1/app.py
+++ aula1/app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template
+from controllers import routes
+
 # Criando a instância do Flask na variável app
 app = Flask(__name__,template_folder='views') # Representa o nome do arquivo que será executado
 
-# Criando a primeira rota da aplicação
-@app.route('/') # o @ é o decorator e faz uma ligação da função que está abaixo dele, ao usuário acessar uma rota
-
-# View Function, função de visualização
-def home():
-    return render_template('index.html')
-
-@app.route('/games')
-def game():
-    return render_template('games.html')
+routes.init_app(app)
 
 # Iniciar o servidor
 if __name__ == '__main__':
